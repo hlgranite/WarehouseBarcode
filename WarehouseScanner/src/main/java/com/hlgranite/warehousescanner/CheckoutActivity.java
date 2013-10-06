639453c907607d23252d5bb0dbf1ec04a093c996
yancyn <yancyn@gmail.com>
2013-10-06 15:58:39 +0800

    Integrate ZXing library with ZXing barcode scanner must installed.

--- WarehouseScanner/src/main/java/com/hlgranite/warehousescanner/CheckoutActivity.java
+++ WarehouseScanner/src/main/java/com/hlgranite/warehousescanner/CheckoutActivity.java
@@ -1,6 +1,5 @@
 package com.hlgranite.warehousescanner;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
@@ -10,13 +9,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import java.util.Date;
-import java.util.Random;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class CheckoutActivity extends Activity {
 
     private FusionManager fusionManager = null;
     private final int MANUAL_ACTIVITY = 2;
+    private final int SCAN_ACTIVITY = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +35,17 @@ public class CheckoutActivity extends Activity {
     protected View.OnClickListener scanClick = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            // TODO: Launch camera for scan barcode
+            // Launch camera for scan barcode
             Log.i("INFO", "Launch camera for scan barcode ");
-            Intent i = new Intent(CheckoutActivity.this, ManualActivity.class);
-            i.putExtra("barcode", "BLUE06003000A13P");
-            startActivityForResult(i, MANUAL_ACTIVITY);
+
+            // see http://damianflannery.wordpress.com/2011/06/13/integrate-zxing-barcode-scanner-into-your-android-app-natively-using-eclipse/
+//            Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+//            intent.putExtra("SCAN_MODE", "CODE_128");
+//            startActivityForResult(intent, SCAN_ACTIVITY);
+
+            // see http://code.google.com/p/zxing/wiki/ScanningViaIntent
+            IntentIntegrator integrator = new IntentIntegrator(CheckoutActivity.this);
+            integrator.initiateScan();
         }
     };
 
@@ -54,12 +60,26 @@ public class CheckoutActivity extends Activity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        Log.i("INFO", "Request code: " + requestCode);
 
         switch(requestCode) {
             case MANUAL_ACTIVITY:
                 Log.i("INFO", "Result code: "+resultCode);
                 if(resultCode == Activity.RESULT_OK) {
                     Toast.makeText(getApplicationContext(), "Checkout successfully!", Toast.LENGTH_LONG).show();
+                }
+                break;
+            //case SCAN_ACTIVITY:
+            default:
+                IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+                if(scanResult != null) {
+                //if(resultCode == Activity.RESULT_OK) {
+                    //String barcode = data.getStringExtra("RESULT");
+                    String barcode = scanResult.getContents();
+                    Log.i("INFO", "Capture barcode: " + barcode);
+                    Intent i = new Intent(CheckoutActivity.this, ManualActivity.class);
+                    i.putExtra("barcode", barcode);
+                    startActivityForResult(i, MANUAL_ACTIVITY);
                 }
                 break;
         }
